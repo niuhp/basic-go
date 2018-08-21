53aa343b70f26f10dda736e884a1cb292cc3cc28
niuhaipeng <Niuhp123>
2018-08-21 19:58:07 +0800

    字典续法全排序

--- src/permutation/dict_ordinal_test.go
+++ src/permutation/dict_ordinal_test.go
@@ -6,16 +6,16 @@ import (
 )
 
 func TestNextPermutation(t *testing.T) {
-	arr := []int{1, 2, 3, 4};
+	arr := []int{1, 2, 3, 4}
 	for arr != nil {
-		fmt.Println(arr);
-		arr = NextPermutation(arr);
+		fmt.Println(arr)
+		arr = NextPermutation(arr)
 	}
 }
 func TestNextPermutation2(t *testing.T) {
-	arr := []int{1, 2, 3, 4,5,6};
+	arr := []int{1, 2, 3, 4, 5, 6}
 	for arr != nil {
-		fmt.Println(arr);
-		arr = NextPermutation(arr);
+		fmt.Println(arr)
+		arr = NextPermutation(arr)
 	}
 }
