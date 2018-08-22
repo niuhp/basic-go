124a0649729b591990c4823f28935e2823205cde
niuhaipeng <Niuhp123>
2018-08-22 15:43:17 +0800

    字典续法全排序

--- src/permutation/dict_ordinal_test.go
+++ src/permutation/dict_ordinal_test.go
@@ -5,15 +5,15 @@ import (
 	"testing"
 )
 
-func TestNextPermutation(t *testing.T) {
+func TestNextPermutation1(t *testing.T) {
 	arr := []int{1, 2, 3, 4}
-	for arr != nil {
-		fmt.Println(arr)
-		arr = NextPermutation(arr)
-	}
+	printAllPermutation(arr);
 }
 func TestNextPermutation2(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 5, 6}
+	printAllPermutation(arr);
+}
+func printAllPermutation(arr []int) {
 	for arr != nil {
 		fmt.Println(arr)
 		arr = NextPermutation(arr)
