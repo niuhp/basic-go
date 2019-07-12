dfd586a20e716c25734746bbed33ceb3b7ba3065
niuhaipeng <Niuhp123>
2019-07-12 17:08:09 +0800

    flag 解析参数

--- src/note/ch1/os_args.go
+++ src/note/ch1/os_args.go
@@ -3,9 +3,17 @@ package main
 import (
 	"os"
 	"fmt"
+	"flag"
 )
 
+var para_a = flag.Bool("a", false, "boolean parameter")
+var para_b = flag.String("b", "para_b_str", "string parameter")
+
 func main() {
+	flag.Parse()
+	fmt.Println("para_a=", *para_a)
+	fmt.Println("para_b=", *para_b)
+	fmt.Println("other_paras=", flag.Args())
 	for index, arg := range os.Args {
 		fmt.Println("args[", index, "] =", arg)
 	}
