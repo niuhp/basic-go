ab1af85c0e95e3e4ba0783b478c1a8f89b02bdd1
niuhaipeng <Niuhp123>
2019-07-18 19:40:46 +0800

    加点注释

--- src/note/ch2/const_test.go
+++ src/note/ch2/const_test.go
@@ -2,11 +2,14 @@ package ch2
 
 import "testing"
 
+/**
+连续赋值，iota 从 0 开始，每出现一次加 1
+ */
 const (
-	State1 = iota + 1
-	State2
-	State3 = iota * 5
-	State4
+	State1 = iota + 1 //iota=0
+	State2            //相当于 iota + 1,iota=1
+	State3 = iota * 5 //iota=2
+	State4            //相当于 iota * 5,iota=3
 )
 
 func TestConst(t *testing.T) {
