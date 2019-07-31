b4dd936a9c5925c9810111166e2c2e666be964db
niuhaipeng <Niuhp123>
2019-07-31 19:37:51 +0800

    for 循环及 switch 语句

--- src/note/statement/statement_test.go
+++ src/note/statement/statement_test.go
@@ -2,18 +2,44 @@ package statement
 
 import (
 	"testing"
-	"fmt"
 )
 
-func TestFor1(t *testing.T) {
+func TestFor(t *testing.T) {
 	for i := 1; i <= 20; i += 3 {
-		fmt.Println(i)
+		t.Log("i =", i)
 	}
 }
-func TestFor2(t *testing.T) {
+func TestWhile(t *testing.T) {
 	i := 5
-	for {
-		fmt.Println(i)
-		i += 5
+	for i < 10 {
+		t.Log("i =", i)
+		i ++
+	}
+}
+func TestMultiCase(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		switch i {
+		case 0, 3, 6, 9:
+			t.Log("i =", i, ",可以被 3 整除")
+			//case 0,7: case 中元素重复时会不能编译
+		case 4, 8:
+			t.Log("i =", i, ",可以被 4 整除")
+		default:
+			t.Log("i =", i, ",不可以被 3 或 4 整除")
+		}
+	}
+}
+func TestConditionCase(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		switch {
+		case i >= 0 && i < 2:
+			t.Log("i =", i, ",i >= 0 && i < 2")
+		case i >= 2 && i < 5: // 条件有重复时只命中第 1 个 case
+			t.Log("i =", i, ",i >= 2 && i < 5")
+		case i >= 3 && i < 6:
+			t.Log("i =", i, ",i >= 3 && i < 6")
+		default:
+			t.Log("i =", i, ",i >= 6")
+		}
 	}
 }
