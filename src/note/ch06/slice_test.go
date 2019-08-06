0b510e054ce802041a28e75ffa4a7ac453b621fe
niuhaipeng <Niuhp123>
2019-08-06 19:38:16 +0800

    切片

--- src/note/ch06/slice_test.go
+++ src/note/ch06/slice_test.go
@@ -1,6 +1,8 @@
 package ch06
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSliceInit(t *testing.T) {
 	//var s0 []int // 默认 cap 为 1，每次扩容为原来的 2 倍
@@ -10,4 +12,23 @@ func TestSliceInit(t *testing.T) {
 		s0 = append(s0, i)
 		t.Log("len =", len(s0), ",cap =", cap(s0), ",s0 =", s0)
 	}
+}
+
+func TestSliceCap(t *testing.T) {
+	nums := [16]int{}
+	for i := 0; i < 16; i++ {
+		nums[i] = i + 1
+	}
+	t.Log(nums)
+
+	s0 := nums[4:9]
+	t.Log("len =", len(s0), "cap =", cap(s0), "s0 =", s0)
+	s0[0] = 100 //会改变原数组 nmms[4] 的值
+	t.Log(nums)
+	s0 = append(s0, 111) //会改变原数组 nmms[9] 的值
+	t.Log(nums)
+	t.Log("len =", len(s0), "cap =", cap(s0), "s0 =", s0)
+
+	s1 := nums[9:12]
+	t.Log("len =", len(s1), "cap =", cap(s1), "s1 =", s1)
 }
