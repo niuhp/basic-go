dfa417d3af5bdbe7fcd6d8aad41b5ce21acf0a1b
niuhaipeng <Niuhp123>
2019-09-04 14:12:36 +0800

    go 语言继承，不能重写函数

---
+++ src/note/ch11/extension_test.go
@@ -0,0 +1,38 @@
+package ch11
+
+import (
+	"fmt"
+	"testing"
+)
+
+type People struct {
+}
+
+func (p *People) Greet() {
+	fmt.Println("hello...")
+}
+
+func (p *People) GreetTo(name string) {
+	p.Greet()
+	fmt.Println(name)
+}
+
+type Man struct {
+	People
+}
+
+func (m *Man) Greet() {
+	fmt.Println("nihao!")
+}
+
+func TestGreet(t *testing.T) {
+	m := new(Man)
+
+	m.GreetTo("tiny") // 可以直接调用 People 定义的函数，但不能覆盖
+
+	m.Greet()
+	m.People.Greet()
+
+	fmt.Println("-------------------")
+	
+}
