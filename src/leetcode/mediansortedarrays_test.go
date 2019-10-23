f19e311585b0074f4736312bb4a87e5a3924878b
niuhaipeng <Niuhp123>
2019-10-23 13:59:36 +0800

    这个文件先存着

---
+++ src/leetcode/mediansortedarrays_test.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	len1, len2 := len(nums1), len(nums2)
+	len := len1 + len2
+	if len%2 == 0 {
+
+	}else{
+		
+	}
+	return 0
+}
